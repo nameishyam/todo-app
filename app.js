f3e38b5253614abd03260dcca4455fd830148e7a
nameishyam <geddamgowtham04@outlook.com>
2024-11-26 23:36:18 +0530

    implement user authentication with passport; add session management and ensure login for todo routes

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
+const passport = require("passport");
+const connectEnsureLogin = require("connect-ensure-login");
+const localStrategy = require("passport-local");
+const session = require("express-session");
 var csrf = require("csurf");
 var cookieParser = require("cookie-parser");
 const { Todo, User } = require("./models");
@@ -13,6 +17,51 @@ app.use(bodyParser.json());
 app.use(cookieParser("im the ceo, bitch!"));
 app.use(csrf({ cookie: true }));
 app.use(express.static(path.join(__dirname, `public`)));
+
+app.use(
+  session({
+    secret: `its my application`,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new localStrategy(
+    {
+      usernameField: `email`,
+      passwordField: `password`,
+    },
+    (username, passowrd, done) => {
+      User.findOne({ where: { email: username, password: password } })
+        .then((user) => {
+          return done(null, user);
+        })
+        .catch((error) => {
+          return error;
+        });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  console.log(`Serializing user in session`, user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
 
 app.set("view engine", "ejs");
 
@@ -29,6 +78,24 @@ app.get(`/`, async (request, response) => {
     });
   }
 });
+
+app.get(
+  `/todos`,
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    const getTodos = await Todo.getAllTodos();
+    if (request.accepts(`html`)) {
+      response.render(`todos`, {
+        getTodos,
+        csrfToken: request.csrfToken(),
+      });
+    } else {
+      response.json({
+        getTodos,
+      });
+    }
+  }
+);
 
 app.get(`/todos`, async (request, response) => {
   console.log(`Todo list`);
@@ -53,7 +120,12 @@ app.post(`/users`, async (request, response) => {
       email: request.body.email,
       password: request.body.password,
     });
-    response.redirect(`/`);
+    request.login(user, (error) => {
+      if (error) {
+        console.log(error);
+      }
+      response.redirect(`/todos`);
+    });
   } catch (error) {
     console.log(error);
   }
